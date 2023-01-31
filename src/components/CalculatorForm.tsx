5a7f36bb36ad059c7fa2899bccbfc3aa175bb889
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2023-01-31 20:54:56 +0200

    added form validation

--- src/components/CalculatorForm.tsx
+++ src/components/CalculatorForm.tsx
@@ -2,26 +2,26 @@ import { useState } from "react"
 import calculateDeliveryFee from "../utils/calculator"
 
 const CalculatorForm = ({updateDeliveryFee}:{updateDeliveryFee:any}) => {
-    const [cartValue, setCartValue] = useState(0)
-    const [deliveryDistance, setDeliveryDistance] = useState(0)
-    const [itemCount, setItemCount] = useState(0)
+    const [cartValue, setCartValue] = useState<string>("")
+    const [deliveryDistance, setDeliveryDistance] = useState<string>("")
+    const [itemCount, setItemCount] = useState<string>("")
 
     // datetime-local input string doesn't contain time zone information so it's removed from the default value
-    const [time, setTime] = useState(new Date().toISOString().split(".")[0])
+    const [time, setTime] = useState<string>(new Date().toISOString().split(".")[0])
 
     const handleCartValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
-        setCartValue(parseFloat(e.target.value.replace(",", ".")) || 0)
+        setCartValue(e.target.value)
     }
     
     const handleDeliveryDistanceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
-        setDeliveryDistance(parseInt(e.target.value) || 0)
+        setDeliveryDistance(e.target.value)
     }
 
     const handleItemCountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
-        setItemCount(parseInt(e.target.value) || 0)
+        setItemCount(e.target.value)
     }
 
     const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,8 +31,13 @@ const CalculatorForm = ({updateDeliveryFee}:{updateDeliveryFee:any}) => {
 
     const handleCalculateDeliveryFee = (e: React.SyntheticEvent) => {
         e.preventDefault()
-        // and time zone (UTC) information is added to the datetime input
-        const deliveryFee = calculateDeliveryFee(cartValue, deliveryDistance, itemCount, new Date(time + ".000Z"))
+        
+        const cartValueAsNumber = parseFloat(cartValue) || 0
+        const deliveryDistanceAsNumber = parseInt(deliveryDistance) || 0
+        const itemCountAsNumber = parseInt(itemCount) || 0
+        
+        // time zone (UTC) information is added to the datetime input
+        const deliveryFee = calculateDeliveryFee(cartValueAsNumber, deliveryDistanceAsNumber, itemCountAsNumber, new Date(time + ".000Z"))
         updateDeliveryFee(deliveryFee.total.toFixed(2))
     }
 
@@ -40,15 +45,15 @@ const CalculatorForm = ({updateDeliveryFee}:{updateDeliveryFee:any}) => {
         <form onSubmit={handleCalculateDeliveryFee}>
             <label>
             Cart value (euros):
-            <input type="number" step="any" min="0" value={cartValue} onChange={handleCartValueChange} />
+            <input type="text" pattern="^[1-9]\d*([.,]{1}\d+)?$" title="Non-negative number required" placeholder="0.00" value={cartValue} onChange={handleCartValueChange} />
             </label><br/>
             <label>
             Delivery distance (meters):
-            <input type="number" min="0" value={deliveryDistance} onChange={handleDeliveryDistanceChange} />
+            <input type="text" pattern="^[1-9]\d*$" title="Non-negative whole number required" placeholder="0" value={deliveryDistance} onChange={handleDeliveryDistanceChange} />
             </label><br/>
             <label>
             Number of items:
-            <input type="number" min="0" value={itemCount} onChange={handleItemCountChange} />
+            <input type="text" pattern="^[1-9]\d*$" title="Non-negative whole number required" placeholder="0" value={itemCount} onChange={handleItemCountChange} />
             </label><br/>
             <label>
             Time:
